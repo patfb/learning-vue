58fa1d2c7c08ca56f5f5ddb4d5f17d0d80239f39
Patrick <patrick.f.boyne@gmail.com>
2018-10-21 01:16:21 -0400

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <list-viewer></list-viewer>>
-    <button-counter customMessage="Hi!"></button-counter>
-    <button-counter customMessage="Hello!"></button-counter>
-    <my-component greeting="Hello"/>
-    <text-reverser></text-reverser>
-    <checkbox-example></checkbox-example>
-    <fading-component></fading-component>
-    
-    <hello-world msg="Welcome to Your Vue.js App"/>
-    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import MyComponent from "./components/MyComponent.vue";
-import ButtonCounter from "./components/ButtonCounter.vue";
-import ListViewer from "./components/ListViewer.vue";
-import TextReverser from "./components/TextReverser.vue";
-import CheckboxExample from "./components/CheckboxExample.vue";
-import FadingComponent from "./components/fading-component.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld,
-    MyComponent,
-    ButtonCounter,
-    ListViewer,
-    TextReverser,
-    CheckboxExample,
-    FadingComponent
-  }
-};
-</script>
 
 <style>
 #app {
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button-counter></button-counter>
+    
+  </div>
+</template>
+
+<script>
+import ButtonCounter from "@/components/ButtonCounter.vue";
+
+export default {
+  name: "about",
+  components: {
+    ButtonCounter
+  }
+};
+</script>
